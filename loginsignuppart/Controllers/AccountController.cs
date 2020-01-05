9f0ee4e58fc62cac1f849a5a71083d0cacdb2396
Devenda Yadav <devendrayadav7676@gmail.com>
2020-01-06 00:32:19 +0530

    Add project files.

---
+++ loginsignuppart/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using loginsignuppart.Models;
+using System.Web.Security;
+
+namespace loginsignuppart.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context=new OfficeEntities())
+            {
+                bool Isvalid = context.User.Any(a => a.UserName == model.UserName && a.Password == model.Password);
+                if (Isvalid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "Invalid username & password");
+                return View();
+            }
+            
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context=new OfficeEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ loginsignuppart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="loginsignuppart.MvcApplication" Language="C#" %>
